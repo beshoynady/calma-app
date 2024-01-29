71e2f5298387bdf0ac391608378204a9ef4e6fd7
beshoynady <bishoin2017@gmail.com>
2024-01-29 22:51:24 +0200

    NavBar

--- client/src/screens/management/manag.component/navbar/NavBar.jsx
+++ client/src/screens/management/manag.component/navbar/NavBar.jsx
@@ -8,23 +8,41 @@ const NavBar = () => {
   return (
     <detacontext.Consumer>
       {
-        ({employeeLoginInfo, employeelogout }) => {
+        ({ employeeLoginInfo, employeelogout }) => {
           return (
-            <header className='manag-header'>
-              <div className='container'>
-                <nav className='manag-nav'>
-                  <div className="profile">
-                    <div className="info">
-                      <p>اهلا, <b>{employeeLoginInfo && employeeLoginInfo.employeeinfo ? employeeLoginInfo.employeeinfo.username :''}</b></p>
-                    </div>
-                    <div className="logout-btn">
-                      <a href='/login' onClick={employeelogout}>خروج</a>
-                    </div>
-                  </div>
+            <nav>
+              <i class='bx bx-menu'></i>
+              <form action="#">
+                <div class="form-input">
+                  <input type="search" placeholder="Search..."/>
+                    <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
+                </div>
+              </form>
+              <input type="checkbox" id="theme-toggle" hidden/>
+                <label for="theme-toggle" class="theme-toggle"></label>
+                <a href="#" class="notif">
+                  <i class='bx bx-bell'></i>
+                  <span class="count">12</span>
+                </a>
+                <a href="#" class="profile">
+                  <img src="images/logo.png"/>
+                </a>
+            </nav>
+            // <header className='manag-header'>
+            //   <div className='container'>
+            //     <nav className='manag-nav'>
+            //       <div className="profile">
+            //         <div className="info">
+            //           <p>اهلا, <b>{employeeLoginInfo && employeeLoginInfo.employeeinfo ? employeeLoginInfo.employeeinfo.username :''}</b></p>
+            //         </div>
+            //         <div className="logout-btn">
+            //           <a href='/login' onClick={employeelogout}>خروج</a>
+            //         </div>
+            //       </div>
 
-                </nav>
-              </div>
-            </header>
+            //     </nav>
+            //   </div>
+            // </header>
           )
         }
       }
