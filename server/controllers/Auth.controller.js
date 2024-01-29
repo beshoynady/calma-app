3c7348fc77e12ae7200b2ca76ac14ec9b38bc3da
beshoynady <bishoin2017@gmail.com>
2024-01-29 21:27:43 +0200

    const login = async (req, res)

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
         return res.status(401).json({ message: 'Incorrect password' });
       }
   
-      const accessToken = generateAccessToken(findUser);
+      // const accessToken = generateAccessToken(findUser);
   
       res.status(200).json({ findUser, accessToken });
     } catch (error) {
