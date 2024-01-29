210e015fa34babed5d6903a2ac8af5620706285e
beshoynady <bishoin2017@gmail.com>
2024-01-30 00:00:27 +0200

    NavBar

--- client/src/screens/management/manag.component/navbar/NavBar.jsx
+++ client/src/screens/management/manag.component/navbar/NavBar.jsx
@@ -12,12 +12,12 @@ const NavBar = () => {
           return (
             <nav>
               {/* <i class='bx bx-menu'></i> */}
-              <form action="#">
+              {/* <form action="#">
                 <div class="form-input">
                   <input type="search" placeholder="Search..."/>
                     <button class="search-btn" type="submit"><i class='bx bx-search'></i></button>
                 </div>
-              </form>
+              </form> */}
               <input type="checkbox" id="theme-toggle" hidden/>
                 <label for="theme-toggle" class="theme-toggle"></label>
                 <a href="#" class="notif">
