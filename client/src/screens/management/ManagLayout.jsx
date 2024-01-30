2c4556a578aa8472996b46b851a2fa3b94d9a87b
beshoynady <bishoin2017@gmail.com>
2024-01-30 13:45:41 +0200

    manag css

--- client/src/screens/management/ManagLayout.jsx
+++ client/src/screens/management/ManagLayout.jsx
@@ -17,9 +17,9 @@ const ManagLayout = () => {
       console.log(decodetoken)
       if (decodetoken.employeeinfo.isActive) {
         return (
-          <div className='manag-body '>
+          <div className='manag-body'>
             <SideBar />
-            <main className='manag_main content'>
+            <main className='content'>
               <NavBar />
               <Outlet></Outlet>
             </main>
