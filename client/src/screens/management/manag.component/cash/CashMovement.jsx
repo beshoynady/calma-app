e8d9c9f0244f3e1b42d5995912e39fdb248a89c6
beshoynady <bishoin2017@gmail.com>
2024-02-02 13:38:22 +0200

    accepteTransferCash

--- client/src/screens/management/manag.component/cash/CashMovement.jsx
+++ client/src/screens/management/manag.component/cash/CashMovement.jsx
@@ -367,22 +367,14 @@ const CashMovement = () => {
                                 <td>{movement.type}</td>
                                 <td>{movement.amount}</td>
                                 <td>{movement.description}</td>
-                                <td>{movement.status == 'Completed'? movement.status :
+                                <td>{movement.status == 'Panding'? 
                                 <>
-                                 <button
-                                 className="btn btn-success"
-                                 onClick={() => { accepteTransferCash(movement._id, 'Completed')}}
-                               >
-                                 قبول
-                               </button>
-                                 <button
-                                 className="btn btn-warning"
-                                 onClick={() => { accepteTransferCash(movement._id), 'Rejected'}}
-                               >
-                                 رفض
-                               </button>
+                                 <button className="btn btn-success" onClick={() => { accepteTransferCash(movement._id, 'Completed')}}
+                               >قبول</button>
+                                 <button className="btn btn-warning" onClick={() => { accepteTransferCash(movement._id), 'Rejected'}}
+                               >رفض</button>
                                 </>
-                                 }</td>
+                                :movement.status }</td>
                                 <td>{new Date(movement.createdAt).toLocaleString('en-GB', { hour12: true })}</td>
                                 {/* <td>
                                   <a href="#editStockactionModal" className="edit" data-toggle="modal" onClick={() => { setactionId(action._id); setoldBalance(action.oldBalance); setoldCost(action.oldCost); setprice(action.price) }}><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
