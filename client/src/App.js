21af7b7b7313e5cfee9b8072e97211a17a3de8db
beshoynady <bishoin2017@gmail.com>
2024-02-05 02:20:28 +0200

    main-color

--- client/src/App.js
+++ client/src/App.js
@@ -727,34 +727,6 @@ function App() {
     }
   }
 
-  // const putNumOfPaid = async (id, numOfPaid) => {
-  //   console.log({ numOfPaid });
-  //   console.log({ list_products: list_products_order });
-
-  //   const arrayofproductorder = list_products_order.map((product) => {
-  //     if (product.productid === id) {
-  //       const oldproduct = list_products_order.find((pro) => pro.productid == id);
-  //       console.log({ oldproduct });
-  //       console.log({ old_numOfPaid: oldproduct.numOfPaid });
-  //       return {
-  //         ...product,
-  //         numOfPaid: oldproduct.numOfPaid + numOfPaid,
-  //       };
-  //     }
-  //     return product;
-  //   });
-  //   const total = arrayofproductorder.map((product) => {
-  //     const subTotal = product.priceAfterDiscount ? product.numOfPaid * product.priceAfterDiscount : product.numOfPaid * product.price;
-  //     return subTotal;
-  //   }, 0);
-
-  //   setsubtotalSplitOrder(total);
-
-  //   console.log({ list_products_order });
-  //   console.log({ arrayofproductorder });
-  //   setnewlistofproductorder([...arrayofproductorder]);
-  //   // calcsubtotalSplitOrder(id, numOfPaid);
-  // };
 
   const putNumOfPaid =  (id, numOfPaid) => {
     console.log({ numOfPaid })
