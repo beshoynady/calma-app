1acb8948d338fa0687405cdcdbb0b682dec083c3
beshoynady <bishoin2017@gmail.com>
2024-02-06 18:13:47 +0200

    content

--- client/src/screens/user.screen/component/contact/Contact.jsx
+++ client/src/screens/user.screen/component/contact/Contact.jsx
@@ -12,21 +12,24 @@ const Contact = () => {
                     <h2>تواصل معنا</h2>
                 </div>
                 <div className='contact-content'>
-                <div className="right">
-                        <p>صفحتنا علي الفيس بوك<br/>
-                            <a href='https://www.facebook.com/calmacafeegy' target="_blank"> <img src={facebook} alt="WhatsApp Icon"/></a>
+                    <div className="right">
+                        <p>صفحتنا علي الفيس بوك<br />
+                            <a href='https://www.facebook.com/calmacafeegy' target="_blank"> <img src={facebook} alt="facebook Icon" /></a>
                         </p>
-                        <p>01144001433 : الموبايل</p>
-                        <p>واتس: <a href="https://api.whatsapp.com/send?phone=01144001433" target="_blank"><img src={whatsapp} alt="WhatsApp Icon"/></a>
-</p>
+                        <p>موبايل
+                            <a href="tel:01144001433">01144001433</a>
+                        </p>
+                        <p> واتساب
+                            <a href="https://api.whatsapp.com/send?phone=01144001433" target="_blank"><img src={whatsapp} alt="WhatsApp Icon" /></a>
+                        </p>
                     </div>
                     <div className="left">
                         <h2></h2>
                         <form action="">
-                            <input placeholder='الاسم' type="text" id='name' required/>
+                            <input placeholder='الاسم' type="text" id='name' required />
                             <input placeholder='E-Mail' type="email" id='email' />
                             <input placeholder='الموبايل' type="tel" id='phone' required />
-                            <textarea placeholder='رسالتك' maxLength={150} type="text" id='supject' required/>
+                            <textarea placeholder='رسالتك' maxLength={150} type="text" id='supject' required />
                             <button type='Submit'>ارسال</button>
                         </form>
                     </div>
