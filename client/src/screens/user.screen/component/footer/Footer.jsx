347f222466a335961f27cf81b3cf971d85c10ca7
beshoynady <bishoin2017@gmail.com>
2024-02-06 23:49:49 +0200

    footer

--- client/src/screens/user.screen/component/footer/Footer.jsx
+++ client/src/screens/user.screen/component/footer/Footer.jsx
@@ -8,20 +8,20 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="footer-logo">
-          <h1>caviar</h1>
+          <h1>CALMA CAFI</h1>
         </div>
         <div className="footer-nav">
         <ul className='foo-nav'>
-            <li><a href="#">Home</a></li>
-            <li><a href="#menu">Menu</a></li>
-            <li><a href="#offer">offers</a></li>
-            <li><a href="#location">Location</a></li>
-            <li><a href="#contact">contact Us</a></li>
+            <li><a href="#">الرئيسية</a></li>
+            <li><a href="#menu">قائمة الطعام</a></li>
+            <li><a href="#offer">العروض</a></li>
+            <li><a href="#location">موقعنا</a></li>
+            <li><a href="#contact">تواصل معنا</a></li>
           </ul>
         </div>
         <div className="footer-social">
           <ul className='foo-soci'>
-            <li><a href=""><img src={face} alt="" /></a></li>
+            <li><a href="https://www.facebook.com/calmacafeegy" target='_blank'><img src={face} alt="facebook" /></a></li>
             <li><a href=""><img src={twit} alt="" /></a></li>
             <li><a href=""><img src={inst} alt="" /></a></li>
           </ul>
