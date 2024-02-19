c5594505f9f41775418c1c7ec2b77f4dadc3d74c
beshoynady <bishoin2017@gmail.com>
2024-02-19 20:10:35 +0200

    contact

--- client/src/screens/user.screen/component/contact/Contact.jsx
+++ client/src/screens/user.screen/component/contact/Contact.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import './Contact.css';
-import whatsapp from '../../../../image/whatsapp.png';
-import facebook from '../../../../image/facebook.png';
+import React from 'react'
+import './Contact.css'
+import whatsapp from '../../../../image/whatsapp.png'
+import facebook from '../../../../image/facebook.png'
 
 const Contact = () => {
+
     return (
         <section className='contact' id='contact'>
             <div className="container">
@@ -11,40 +12,32 @@ const Contact = () => {
                     <h2>تواصل معنا</h2>
                 </div>
                 <div className='contact-content'>
-                    <div className="contact-info">
-                        <h3>معلومات الاتصال</h3>
-                        <div className="social-media">
-                            <a href='https://www.facebook.com/calmacafeegy' target="_blank" rel="noreferrer">
-                                <img src={facebook} alt="Facebook Icon" />
-                            </a>
-                            <a href="https://api.whatsapp.com/send?phone=+201144001433" target="_blank" rel="noreferrer">
-                                <img src={whatsapp} alt="WhatsApp Icon" />
-                            </a>
-                        </div>
-                        <p className="phone">موبايل: <a href="tel:01144001433">01144001433</a></p>
+                    <div className="right">
+                        <p>صفحتنا علي الفيس بوك<br />
+                            <a href='https://www.facebook.com/calmacafeegy' target="_blank"  rel="noreferrer"> <img src={facebook} alt="facebook Icon" /></a>
+                        </p>
+                        <p> واتساب
+                            <a href="https://api.whatsapp.com/send?phone=+201144001433" target="_blank"  rel="noreferrer"><img src={whatsapp} alt="WhatsApp Icon" /></a>
+                        </p>
+                        <p>موبايل
+                            <a href="tel:01144001433">01144001433</a>
+                        </p>
                     </div>
-                    <div className="contact-form">
-                        <h3>أرسل لنا رسالتك</h3>
+                    <div className="left">
+                        <h2>لارسال الشكاوي و الملاحظات</h2>
                         <form action="">
-                            <div className="form-group">
-                                <input placeholder='الاسم' type="text" id='name' required />
-                            </div>
-                            <div className="form-group">
-                                <input placeholder='البريد الإلكتروني' type="email" id='email' required />
-                            </div>
-                            <div className="form-group">
-                                <input placeholder='الموبايل' type="tel" id='phone' required />
-                            </div>
-                            <div className="form-group">
-                                <textarea placeholder='رسالتك' maxLength={150} id='supject' required />
-                            </div>
+                            <input placeholder='الاسم' type="text" id='name' required />
+                            <input placeholder='E-Mail' type="email" id='email' />
+                            <input placeholder='الموبايل' type="tel" id='phone' required />
+                            <textarea placeholder='رسالتك' maxLength={150} type="text" id='supject' required />
                             <button type='Submit'>ارسال</button>
                         </form>
                     </div>
+
                 </div>
             </div>
         </section>
     )
 }
 
-export default Contact;
+export default Contact
