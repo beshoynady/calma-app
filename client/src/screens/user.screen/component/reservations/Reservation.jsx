fbd090f2bc6c8d757c122545f37024584856e8d4
beshoynady <bishoin2017@gmail.com>
2024-02-23 17:59:01 +0200

    setStartTime

--- client/src/screens/user.screen/component/reservations/Reservation.jsx
+++ client/src/screens/user.screen/component/reservations/Reservation.jsx
@@ -53,11 +53,11 @@ const Reservation = () => {
                   </div>
                   <div className="col-md-3 mb-1">
                     <label htmlFor="arrivalTime" className="form-label">وقت الحضور</label>
-                    <input type="time" className="form-control" id="arrivalTime" onChange={(e)=>setStartTime(e.target.value)} />
+                    <input type="time" className="form-control" id="arrivalTime" onChange={(e)=>setStartTime(new Date(e.target.value))} />
                   </div>
                   <div className="col-md-3 mb-1">
                     <label htmlFor="departureTime" className="form-label">وقت الانصراف</label>
-                    <input type="time" className="form-control" id="departureTime" onChange={(e)=>setEndTime(e.target.value)} />
+                    <input type="time" className="form-control" id="departureTime" onChange={(e)=>setEndTime(new Date(e.target.value))} />
                   </div>
                 </div>
                 <div className="mb-1">
