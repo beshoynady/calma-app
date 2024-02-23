022cef559c3d287e03efe3b3f0294a890c3ddb95
beshoynady <bishoin2017@gmail.com>
2024-02-23 19:17:40 +0200

    selectedTime

--- client/src/screens/user.screen/component/reservations/Reservation.jsx
+++ client/src/screens/user.screen/component/reservations/Reservation.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { detacontext } from '../../../../App';
 import { ToastContainer } from 'react-toastify';
 
@@ -13,12 +13,12 @@ const Reservation = () => {
 
   return (
     <detacontext.Consumer>
-      {({ allTable, createReservations, updateReservation, getAllReservations , allReservations , getReservationById,deleteReservation,userLoginInfo }) => {
-        console.log({userLoginInfo})
+      {({ allTable, createReservations, updateReservation, getAllReservations, allReservations, getReservationById, deleteReservation, userLoginInfo }) => {
+        console.log({ userLoginInfo })
         const userId = userLoginInfo?.userinfo?.id;
         return (
           <div id='reservation' className='d-flex align-items-center flex-column justify-content-start' style={{ height: 'calc(100vh - 80px)', width: '100%' }}>
-            <ToastContainer/>
+            <ToastContainer />
             <div className='section-title'>
               <h2>حجز طاولة</h2>
             </div>
@@ -29,21 +29,21 @@ const Reservation = () => {
               paddingBottom: '50px',
             }}>
               <form className="w-100 text-white" style={{ fontSize: '20px', fontWeight: '800' }}
-              onSubmit={(e)=>createReservations(e, tableId, userId ,customerName,customerPhone , reservationDate, startTime, endTime, reservationNote)}>
+                onSubmit={(e) => createReservations(e, tableId, userId, customerName, customerPhone, reservationDate, startTime, endTime, reservationNote)}>
                 <div className="mb-1">
                   <label htmlFor="name" className="form-label">الاسم</label>
-                  <input type="text" className="form-control" id="name" onChange={(e)=>setCustomerName(e.target.value)} />
+                  <input type="text" className="form-control" id="name" onChange={(e) => setCustomerName(e.target.value)} />
                 </div>
                 <div className="row mb-1">
                   <div className="col-md-6">
                     <label htmlFor="mobile" className="form-label">رقم الموبايل</label>
-                    <input type="tel" className="form-control" id="mobile" onChange={(e)=>setCustomerPhone(e.target.value)} />
+                    <input type="tel" className="form-control" id="mobile" onChange={(e) => setCustomerPhone(e.target.value)} />
                   </div>
                   <div className="col-md-6">
                     <label htmlFor="tableNumber" className="form-label">رقم الطاولة</label>
-                    <select className="form-control" id="tableNumber" onChange={(e)=>setTableId(e.target.value)}>
-                        <option>اختار رقم الطاوله</option> 
-                      {allTable.map((table,i) => (
+                    <select className="form-control" id="tableNumber" onChange={(e) => setTableId(e.target.value)}>
+                      <option>اختار رقم الطاوله</option>
+                      {allTable.map((table, i) => (
                         <option key={i} value={table._id}>{table.tablenum}</option>
                       ))}
                     </select>
@@ -52,20 +52,32 @@ const Reservation = () => {
                 <div className="row">
                   <div className="col-md-6 mb-1">
                     <label htmlFor="date" className="form-label">التاريخ</label>
-                    <input type="date" className="form-control" id="date" onChange={(e)=>setReservationDate(e.target.value)} />
+                    <input type="date" className="form-control" id="date" onChange={(e) => setReservationDate(e.target.value)} />
                   </div>
                   <div className="col-md-3 mb-1">
                     <label htmlFor="arrivalTime" className="form-label">وقت الحضور</label>
-                    <input type="time" className="form-control" id="arrivalTime" onChange={(e)=>setStartTime(e.target.value)} />
+                    <input type="time" className="form-control" id="arrivalTime" onChange={(e) => {
+                      const selectedTime = new Date(); // إنشاء كائن Date
+                      const timeParts = e.target.value.split(':'); // تقسيم الوقت إلى ساعات ودقائق
+                      selectedTime.setHours(parseInt(timeParts[0])); // تعيين الساعات
+                      selectedTime.setMinutes(parseInt(timeParts[1])); // تعيين الدقائق
+                      setStartTime(selectedTime); // تعيين الوقت
+                    }} />
                   </div>
                   <div className="col-md-3 mb-1">
                     <label htmlFor="departureTime" className="form-label">وقت الانصراف</label>
-                    <input type="time" className="form-control" id="departureTime" onChange={(e)=>setEndTime(e.target.value)} />
+                    <input type="time" className="form-control" id="departureTime" oonChange={(e) => {
+                      const selectedTime = new Date(); // إنشاء كائن Date
+                      const timeParts = e.target.value.split(':'); // تقسيم الوقت إلى ساعات ودقائق
+                      selectedTime.setHours(parseInt(timeParts[0])); // تعيين الساعات
+                      selectedTime.setMinutes(parseInt(timeParts[1])); // تعيين الدقائق
+                      setStartTime(selectedTime); // تعيين الوقت
+                    }} />
                   </div>
                 </div>
                 <div className="mb-1">
                   <label htmlFor="notes" className="form-label">ملاحظات</label>
-                  <textarea className="form-control" id="notes" rows="2" onChange={(e)=>setReservationNote(e.target.value)}></textarea>
+                  <textarea className="form-control" id="notes" rows="2" onChange={(e) => setReservationNote(e.target.value)}></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary" style={{ width: '100%', height: '50px' }}>تأكيد الحجز</button>
               </form>
