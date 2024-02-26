57d8da05e419d1dd21d996e977ee0f6ca5610f95
beshoynady <bishoin2017@gmail.com>
2024-02-26 21:49:58 +0200

    updatereservationModal

--- client/src/screens/management/manag.component/tables/ReservationTables.jsx
+++ client/src/screens/management/manag.component/tables/ReservationTables.jsx
@@ -9,6 +9,7 @@ import { ToastContainer, toast } from 'react-toastify';
 const ReservationTables = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
 
+  const [reservationId, setReservationId] = useState('');
   const [customerName, setCustomerName] = useState('');
   const [customerPhone, setCustomerPhone] = useState('');
   const [reservationNote, setReservationNote] = useState('');
@@ -110,7 +111,7 @@ const ReservationTables = () => {
     <detacontext.Consumer>
       {
         ({ EditPagination, startpagination, endpagination, setstartpagination, setendpagination, createReservations, confirmReservation, updateReservation, getAllReservations, allReservations, getReservationById, deleteReservation, employeeLoginInfo, allusers,
-        formatDate, formatTime }) => {
+          formatDate, formatTime }) => {
 
           const createBy = employeeLoginInfo?.employeeinfo?.id;
           return (
@@ -121,7 +122,7 @@ const ReservationTables = () => {
                   <div className="table-title">
                     <div className="row">
                       <div className="col-sm-6">
-                        <h2>ادارة <b>الطاولات</b></h2>
+                        <h2>ادارة <b>حجز الطاولات</b></h2>
                       </div>
                       <div className="col-sm-6 d-flex justify-content-end">
                         <a href="#createreservationModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>انشاء حجز جديد</span></a>
@@ -169,12 +170,6 @@ const ReservationTables = () => {
                   <table className="table table-striped table-hover">
                     <thead>
                       <tr>
-                        <th>
-                          <span className="custom-checkbox">
-                            <input type="checkbox" id="selectAll" />
-                            <label htmlFor="selectAll"></label>
-                          </span>
-                        </th>
                         <th>م</th>
                         <th>رقم الطاولة</th>
                         <th>الاسم</th>
@@ -229,18 +224,6 @@ const ReservationTables = () => {
                           if (i >= startpagination & i < endpagination) {
                             return (
                               <tr key={i}>
-                                <td>
-                                  <span className="custom-checkbox">
-                                    <input
-                                      type="checkbox"
-                                      id={`checkbox${i}`}
-                                      name="options[]"
-                                      value={reservation._id}
-                                    // onChange={handleCheckboxChange}
-                                    />
-                                    <label htmlFor={`checkbox${i}`}></label>
-                                  </span>
-                                </td>
                                 <td>{i + 1}</td>
                                 <td>{reservation.tableNum}</td>
                                 <td>{reservation.customerName}</td>
@@ -259,12 +242,8 @@ const ReservationTables = () => {
                                   </select>
                                 </td>
                                 <td>
-                                  <a href="#editTableModal" className="edit" data-toggle="modal"
+                                  <a href="#updatereservationModal" className="edit" data-toggle="modal" onClick={(e) => {setReservationId(reservation._id) ,setCustomerName(reservation.customerName); setCustomerPhone(reservation.customerPhone); setNumberOfGuests(reservation.numberOfGuests); setEndTime(reservation.endTime); setStartTime(reservation.startTime); setReservationDate(reservation.startDate); setReservationNote(reservation.reservationNotes); setTableInfo({ id: reservation.tableId, tablenum: reservation.tableNum }) }}
                                   ><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-
-                                  <a href="#deleteTableModal" className="delete" data-toggle="modal"
-                                  >
-                                    <i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                                 </td>
                               </tr>
                             )
@@ -299,7 +278,12 @@ const ReservationTables = () => {
                         <div className="container">
                           <div className="mb-1">
                             <label htmlFor="name" className="form-label">الاسم</label>
-                            <input type="text" className="form-control" id="name" onChange={(e) => setCustomerName(e.target.value)} />
+                            <input type="text" className="form-control" id="name" onChange={(e) => clientByName(e.target.value)} />
+                            <ul>
+                              {filteredClients && filteredClients.map((client, index) => (
+                                <li key={index}>{client.username}</li>
+                              ))}
+                            </ul>
                           </div>
                           <div className="row mb-1">
                             <div className="col-md-6">
@@ -310,7 +294,7 @@ const ReservationTables = () => {
                               <label htmlFor="tableNumber" className="form-label">رقم الطاولة</label>
                               <select className="form-control" id="tableNumber" onChange={(e) => setTableInfo({ id: e.target.value, tablenum: e.target.options[e.target.selectedIndex].text })}>
                                 <option>اختار رقم الطاوله</option>
-                                {listoftable.map((table, i) => (
+                                {listoftable && listoftable.map((table, i) => (
                                   <option key={i} value={table._id}>{table.tablenum}</option>
                                 ))}
                               </select>
@@ -404,88 +388,130 @@ const ReservationTables = () => {
                   </div>
                 </div >
               </div >
-              {/* {tableid && <div id="editTableModal" className="modal fade">
-                <div className="modal-dialog">
+
+
+              <div id="updatereservationModal" className="modal fade">
+                <div className="modal-dialog modal-dialog-centered modal-lg">
                   <div className="modal-content">
-                    <form onSubmit={editTable}>
+                    <form onSubmit={(e) => updateReservation(e, reservationId, tableInfo.id, tableInfo.tablenum, userId, numberOfGuests, customerName, customerPhone, reservationDate, startTime, endTime, reservationNote, createBy)}>
                       <div className="modal-header">
-                        <h4 className="modal-title">تعديل طاولة</h4>
+                        <h4 className="modal-title">اضافه حجز طاولة</h4>
                         <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                       </div>
                       <div className="modal-body">
-                        <div className="form-group">
-                          <label>رقم السكشن</label>
-                          <input type="Number" className="form-control" required onChange={(e) => setsectionNumber(e.target.value)} />
-                        </div>
-                        <div className="form-group">
-                          <label>رقم الطاولة</label>
-                          <input type="Number" defaultValue={allReservations.length > 0 ? listoftable[allReservations.length - 1].tablenum : ""} className="form-control" required onChange={(e) => settablenum(e.target.value)} />
-                        </div>
-                        <div className="form-group">
-                          <label>عدد المقاعد</label>
-                          <input type="Number" defaultValue={allReservations.length > 0 ? allReservations.find((table, i) => reservation._id == tableid).chairs : ''} className="form-control" required onChange={(e) => setchairs(e.target.value)} />
-                        </div>
-                        <div className="form-group">
-                          <label>الوصف</label>
-                          <textarea defaultValue={allReservations.length > 0 ? allReservations.find((table, i) => reservation._id == tableid).description : ""} className="form-control" required onChange={(e) => settabledesc(e.target.value)}></textarea>
-                        </div>
-                        <div className="form-group">
-                          <label>متاح</label>
-                          <select name="category" id="category" form="carform" onChange={(e) => setisValid(e.target.value)}>
-                            <option >اختر</option>
-                            <option value={true} >متاح</option>
-                            <option value={false} >غير متاح</option>
-                          </select>
+                        <div className="container">
+                          <div className="mb-1">
+                            <label htmlFor="name" className="form-label">الاسم</label>
+                            <input type="text" className="form-control" id="name" defaultValue={clientByName} onChange={(e) => clientByName(e.target.value)} />
+                            <ul>
+                              {filteredClients && filteredClients.map((client, index) => (
+                                <li key={index}>{client.username}</li>
+                              ))}
+                            </ul>
+                          </div>
+                          <div className="row mb-1">
+                            <div className="col-md-6">
+                              <label htmlFor="mobile" className="form-label">رقم الموبايل</label>
+                              <input type="tel" className="form-control" id="mobile" defaultValue={customerPhone} onChange={(e) => setCustomerPhone(e.target.value)} />
+                            </div>
+                            <div className="col-md-3">
+                              <label htmlFor="tableNumber" className="form-label">رقم الطاولة</label>
+                              <select className="form-control" id="tableNumber" defaultValue={tableInfo.tablenum} onChange={(e) => setTableInfo({ id: e.target.value, tablenum: e.target.options[e.target.selectedIndex].text })}>
+                                <option>{tableInfo.tablenum}</option>
+                                {listoftable && listoftable.map((table, i) => (
+                                  <option key={i} value={table._id}>{table.tablenum}</option>
+                                ))}
+                              </select>
+                            </div>
+                            <div className="col-md-3">
+                              <label htmlFor="numberOfGuests" className="form-label">عدد الضيوف</label>
+                              <input type="number" className="form-control" id="numberOfGuests" defaultValue={numberOfGuests} onChange={(e) => setNumberOfGuests(e.target.value)} />
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-md-6 mb-1">
+                              <label htmlFor="date" className="form-label">التاريخ</label>
+                              <input
+                                type="date"
+                                className="form-control"
+                                id="date"
+                                defaultValue={reservationDate} onChange={(e) => {
+                                  const selectedDate = new Date(e.target.value);
+                                  setReservationDate(selectedDate);
+                                }}
+                              />
+                            </div>
+                            <div className="col-md-3 mb-1">
+                              <label htmlFor="arrivalTime" className="form-label">وقت الحضور</label>
+                              <input
+                                type="time"
+                                className="form-control"
+                                id="arrivalTime"
+                                required
+                                defaultValue={startTime} onChange={(e) => {
+                                  setStartTimeClicked(true);
+                                  if (reservationDate) {
+                                    const StartedDate = new Date(reservationDate);
+                                    const timeParts = e.target.value.split(':');
+                                    console.log({ timeParts })
+                                    if (StartedDate) {
+                                      StartedDate.setHours(parseInt(timeParts[0]));
+                                      StartedDate.setMinutes(parseInt(timeParts[1]));
+                                      console.log({ StartedDate })
+                                      setStartTime(StartedDate);
+                                    }
+                                  } else {
+                                    e.target.value = ''
+                                  }
+                                }}
+                              />
+                              {startTimeClicked && !reservationDate && (
+                                <div style={{ color: 'red', fontSize: "18px", marginTop: '0.5rem' }}>يرجى تحديد التاريخ أولاً</div>
+                              )}
+                            </div>
+                            <div className="col-md-3 mb-1">
+                              <label htmlFor="departureTime" className="form-label">وقت الانصراف</label>
+                              <input
+                                type="time"
+                                className="form-control"
+                                id="departureTime"
+                                required
+                                defaultValue={endTime} onChange={(e) => {
+                                  setEndTimeClicked(true);
+                                  if (reservationDate) {
+                                    const EndedDate = new Date(reservationDate);
+                                    const timeParts = e.target.value.split(':');
+                                    console.log({ timeParts })
+                                    if (EndedDate) {
+                                      EndedDate.setHours(parseInt(timeParts[0]));
+                                      EndedDate.setMinutes(parseInt(timeParts[1]));
+                                      console.log({ EndedDate })
+                                      setEndTime(EndedDate);
+                                    }
+                                  } else {
+                                    e.target.value = ''
+                                  }
+                                }}
+                              />
+                              {endTimeClicked && !reservationDate && (
+                                <div style={{ color: 'red', fontSize: "18px", marginTop: '0.5rem' }}>يرجى تحديد التاريخ أولاً</div>
+                              )}
+                            </div>
+                          </div>
+                          <div className="mb-1">
+                            <label htmlFor="notes" className="form-label">ملاحظات</label>
+                            <textarea className="form-control" id="notes" rows="2" defaultValue={reservationNote} onChange={(e) => setReservationNote(e.target.value)}></textarea>
+                          </div>
                         </div>
                       </div>
                       <div className="modal-footer">
+                        <input type="submit" className="btn btn-success" value="ضافه" />
                         <input type="button" className="btn btn-danger" data-dismiss="modal" value="إغلاق" />
-                        <input type="submit" className="btn btn-info" value="حفظ" />
                       </div>
                     </form>
                   </div>
-                </div>
-              </div>}
-              <div id="deleteTableModal" className="modal fade">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <form onSubmit={deleteTable}>
-                      <div className="modal-header">
-                        <h4 className="modal-title">حذف طاولة</h4>
-                        <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                      </div>
-                      <div className="modal-body">
-                        <p>هل انت متاكد من حذف هذا السجل؟?</p>
-                        <p className="text-warning"><small>لا يمكن الرجوع في هذا الاجراء.</small></p>
-                      </div>
-                      <div className="modal-footer">
-                        <input type="button" className="btn btn-danger" data-dismiss="modal" value="إغلاق" />
-                        <input type="submit" className="btn btn-danger" value="حذف" />
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-              <div id="deleteListTableModal" className="modal fade">
-                <div className="modal-dialog">
-                  <div className="modal-content">
-                    <form onSubmit={deleteSelectedIds}>
-                      <div className="modal-header">
-                        <h4 className="modal-title">حذف طاولة</h4>
-                        <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                      </div>
-                      <div className="modal-body">
-                        <p>هل انت متاكد من حذف هذا السجل؟?</p>
-                        <p className="text-warning"><small>لا يمكن الرجوع في هذا الاجراء.</small></p>
-                      </div>
-                      <div className="modal-footer">
-                        <input type="button" className="btn btn-danger" data-dismiss="modal" value="إغلاق" />
-                        <input type="submit" className="btn btn-danger" value="حذف" />
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div> */}
+                </div >
+              </div >
             </div >
           )
         }
