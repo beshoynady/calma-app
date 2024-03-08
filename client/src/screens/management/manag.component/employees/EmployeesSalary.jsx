d72eee4935b65dc47627b937874111a954e7dcc3
beshoynady <bishoin2017@gmail.com>
2024-03-08 12:23:41 +0200

    EmployeesSalary

--- client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
+++ client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
@@ -7,7 +7,6 @@ const Joi = require('joi');
 
 const EmployeesSalary = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
-  const token = localStorage.getItem('token_e'); // Retrieve the token from localStorage
 
   
   // Existing state variables and useEffect
