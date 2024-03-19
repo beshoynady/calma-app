c79bee4980cd6e1e1da01e969d0a34928cb25b9d
beshoynady <bishoin2017@gmail.com>
2024-03-19 17:37:52 +0200

    restaurantSchema

--- server/models/Restaurant.model.js
+++ server/models/Restaurant.model.js
@@ -18,71 +18,71 @@ const restaurantSchema = new mongoose.Schema({
         street: String,
         postal_code: String,
     },
-    contact: {
-        phone: [{
-            type: String,
-            match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
-        }],
-        whatsapp: {
-            type: String,
-            match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
-        },
-        email: {
-            type: String,
-            lowercase: true, 
-            trim: true,
-            unique: true, 
-            match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], 
-        },
-        social_media: {
-            facebook: String,
-            twitter: String,
-            instagram: String,
-            linkedin: String,
-            youtube: String,
-        }
-    },
-    opening_hours: {
-        Saturday: {
-            from: String,
-            to: String,
-            closed: Boolean 
-        },
-        Sunday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        },
-        Monday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        },
-        Tuesday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        },
-        Wednesday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        },
-        Thursday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        },
-        Friday: {
-            from: String,
-            to: String,
-            closed: Boolean
-        }
-    },
-    delivery:  {
-        type: Boolean,
-        default: false
-    }
+    // contact: {
+    //     phone: [{
+    //         type: String,
+    //         match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
+    //     }],
+    //     whatsapp: {
+    //         type: String,
+    //         match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
+    //     },
+    //     email: {
+    //         type: String,
+    //         lowercase: true, 
+    //         trim: true,
+    //         unique: true, 
+    //         match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], 
+    //     },
+    //     social_media: {
+    //         facebook: String,
+    //         twitter: String,
+    //         instagram: String,
+    //         linkedin: String,
+    //         youtube: String,
+    //     }
+    // },
+    // opening_hours: {
+    //     Saturday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean 
+    //     },
+    //     Sunday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     },
+    //     Monday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     },
+    //     Tuesday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     },
+    //     Wednesday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     },
+    //     Thursday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     },
+    //     Friday: {
+    //         from: String,
+    //         to: String,
+    //         closed: Boolean
+    //     }
+    // },
+    // delivery:  {
+    //     type: Boolean,
+    //     default: false
+    // }
 });
 
 const RestaurantModel = mongoose.model('Restaurant', restaurantSchema);
