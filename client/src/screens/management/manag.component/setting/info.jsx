a81ab991309c18d377361bd216f1dbae5d854e90
beshoynady <bishoin2017@gmail.com>
2024-03-19 21:43:53 +0200

    handleContactData

--- client/src/screens/management/manag.component/setting/info.jsx
+++ client/src/screens/management/manag.component/setting/info.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { detacontext } from '../../../../App';
 import { toast } from 'react-toastify';
