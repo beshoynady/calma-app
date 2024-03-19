810f40f51576702c12ed733ce0c20dd521d30f7e
beshoynady <bishoin2017@gmail.com>
2024-03-19 18:09:02 +0200

    restaurantSchema

--- server/models/Restaurant.model.js
+++ server/models/Restaurant.model.js
@@ -18,30 +18,30 @@ const restaurantSchema = new mongoose.Schema({
         street: String,
         postal_code: String,
     },
-    // contact: {
-    //     phone: [{
-    //         type: String,
-    //         match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
-    //     }],
-    //     whatsapp: {
-    //         type: String,
-    //         match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
-    //     },
-    //     email: {
-    //         type: String,
-    //         lowercase: true, 
-    //         trim: true,
-    //         unique: true, 
-    //         match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], 
-    //     },
-    //     social_media: {
-    //         facebook: String,
-    //         twitter: String,
-    //         instagram: String,
-    //         linkedin: String,
-    //         youtube: String,
-    //     }
-    // },
+    contact: {
+        phone: [{
+            type: String,
+            match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
+        }],
+        whatsapp: {
+            type: String,
+            match: [/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Please enter a valid phone number'],
+        },
+        email: {
+            type: String,
+            lowercase: true, 
+            trim: true,
+            // unique: true, 
+            match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'], 
+        },
+        social_media: {
+            facebook: String,
+            twitter: String,
+            instagram: String,
+            linkedin: String,
+            youtube: String,
+        }
+    },
     opening_hours: {
         Saturday: {
             from: String,
