88044b1207c11de3a10db49d7c7df1e4a97b5bc2
beshoynady <bishoin2017@gmail.com>
2024-03-29 16:42:32 +0200

    Employees

--- client/src/screens/management/manag.component/employees/Employees.jsx
+++ client/src/screens/management/manag.component/employees/Employees.jsx
@@ -384,7 +384,7 @@ const Employees = () => {
                               <td>{emp.basicSalary}</td>
                               <td>{emp.isActive ? 'متاح' : "غير متاح"}</td>
                               <td>{emp.sectionNumber}</td>
-                              <td>{emp.shift}</td>
+                              <td>{emp.shift.shiftType}</td>
                               <td>{new Date(emp.createdAt).toLocaleString('en-GB', { hour12: true })}</td>
                               <td>
                                 <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit" onClick={() => {
@@ -422,7 +422,7 @@ const Employees = () => {
                                 <td>{emp.basicSalary}</td>
                                 <td>{emp.isActive ? 'متاح' : "غير متاح"}</td>
                                 <td>{emp.sectionNumber}</td> 
-                                <td>{emp.shift}</td>
+                                <td>{emp.shift.shiftType}</td>
                                 <td>{new Date(emp.createdAt).toLocaleString('en-GB', { hour12: true })}</td>
                                 <td>
                                   <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit" onClick={() => {
