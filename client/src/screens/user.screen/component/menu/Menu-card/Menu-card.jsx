6c1e65b7d961af65f119040963e742416cae67a1
beshoynady <bishoin2017@gmail.com>
2024-04-02 21:27:28 +0200

    MenuCard

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -13,51 +13,8 @@ const MenuCard = () => {
         ({ allProducts, categoryid, addItemToCart, deleteItemFromCart, incrementProductQuantity, decrementProductQuantity, setproductNote, addNoteToProduct, itemId }) => {
           return (
             <div className="card-group">
-              {allProducts.length > 0 ? allProducts.filter(pro => pro.category === categoryid).map((product, index) => {
-                product.hasSizes?
-                 (
-                  <div className="menu-card" key={index}>
-                    <img className='img-card' src={product.image ? `${apiUrl}/images/${product.image}` : ""} alt="" />
-                    {product._id == productid & noteArea == true ? <form onSubmit={(e) => { addNoteToProduct(e, product._id); setnoteArea(!noteArea) }}>
-                      <textarea placeholder='اضف تعليماتك الخاصة بهذا الطبق' name="note" cols="100" rows="3" onChange={(e) => { setproductNote(e.target.value) }}></textarea>
-                      <div className='note-btn'>
-                        <button>تاكيد</button>
-                        <button onClick={() => setnoteArea(!noteArea)}>الغاء</button>
-                      </div>
-                    </form> : ''}
-
-                    <div className="detalis">
-                      <div className='product-det'>
-                        <div className='product-name'>
-                          <h2>{product.name}</h2>
-                          <span className="material-symbols-outlined" onClick={() => { setnoteArea(!noteArea); setproductid(product._id) }}>note_alt</span>
-                        </div>
-                        <p>{product.description}</p>
-                      </div>
-                      <div className="price">
-                        <div className="counter">
-                          <button className='symb' onClick={() => decrementProductQuantity(product._id)}>-</button>
-                          <span className='num'>{product.quantity}</span>
-                          <button className='symb' onClick={() => incrementProductQuantity(product._id)}>+</button>
-                        </div>
-                        {product.discount > 0 ?
-                          <p><sup><del>{product.price}</del></sup>{product.price - product.discount}ج</p> :
-                          <p>{product.price} ج</p>}
-                      </div>
-                      {product.avaliable ?
-                        <div className='card-btn'>
-                          {itemId.filter((i) => i === product._id).length > 0 && product.quantity > 0 ?
-                            <button type="button" className='btn btn-danger delfromcart' onClick={() => { deleteItemFromCart(product._id) }}>احذف من الطلبات</button>
-                            : <button type="button" className='btn btn-success addtocart' onClick={() => { if (product.quantity > 0) { addItemToCart(product._id) } }}> اضف الي طلباتي</button>
-                          }
-                        </div>
-                        : <div className='card-btn'>
-                          <button type="button" className='btn btn-warning delfromcart'>غير متاح الآن</button>
-                        </div>
-                      }
-                    </div>
-                  </div>
-                ):                 (
+              {allProducts.length > 0 ? allProducts.filter(pro => pro.category._id === categoryid).map((product, index) => {
+                return (
                   <div className="menu-card" key={index}>
                     <img className='img-card' src={product.image ? `${apiUrl}/images/${product.image}` : ""} alt="" />
                     {product._id == productid & noteArea == true ? <form onSubmit={(e) => { addNoteToProduct(e, product._id);; setnoteArea(!noteArea) }}>
@@ -100,8 +57,6 @@ const MenuCard = () => {
                     </div>
                   </div>
                 )
-                  
-                
               }
               ) : ''}
             </div>
