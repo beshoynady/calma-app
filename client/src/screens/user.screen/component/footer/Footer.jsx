adddc6c4d3da94aa4cc7915f3456adb8a8d16933
bishoin2017@gmail.com <bishoin2017@gmail.com>
2024-05-23 14:43:25 +0300

    Footer

--- client/src/screens/user.screen/component/footer/Footer.jsx
+++ client/src/screens/user.screen/component/footer/Footer.jsx
@@ -2,66 +2,37 @@ import React from 'react'
 // import './Footer.css';
 import { detacontext } from '../../../../App'
 
-import face from '../../../../image/social/facebook.svg'
-import twit from '../../../../image/social/twitter.svg'
-import inst from '../../../../image/social/square-instagram.svg'
 const Footer = () => {
   return (
     <detacontext.Consumer>
       {
         ({ restaurantData, askingForHelp, userLoginInfo, usertitle }) => {
           return (
-            // <footer>
-            // <div classNameName="container">
-            //   <div classNameName="footer-logo">
-            //     <h1>CALMA CAFI</h1>
-            //   </div>
-            //   <div classNameName="footer-nav">
-            //   <ul classNameName='foo-nav'>
-            //       <li><a href="#">الرئيسية</a></li>
-            //       <li><a href="#menu">قائمة الطعام</a></li>
-            //       <li><a href="#offer">العروض</a></li>
-            //       <li><a href="#location">موقعنا</a></li>
-            //       <li><a href="#contact">تواصل معنا</a></li>
-            //     </ul>
-            //   </div>
-            //   <div classNameName="footer-social">
-            //     <ul classNameName='foo-soci'>
-            //       <li><a href="https://www.facebook.com/calmacafeegy" target='_blank'><img src={face} alt="facebook" /></a></li>
-            //       <li><a href=""><img src={twit} alt="" /></a></li>
-            //       <li><a href=""><img src={inst} alt="" /></a></li>
-            //     </ul>
-            //   </div>
-            // </div>
             <div className="container bottom-0">
-              <footer className="text-white text-center text-lg-start" style={{ backgroundColor: "#23242a" }}>
+              <footer className="text-white text-center text-lg-start" style={{ backgroundColor: "#23242a", direction: "rtl" }}>
                 <div className="container p-4">
                   <div className="row mt-4">
                     <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
                       <h5 className="text-uppercase mb-4">{restaurantData.name}</h5>
-
                       <p>{restaurantData.description}</p>
-
                       <div className="mt-4">
                         {restaurantData.social_media && restaurantData.social_media.map((item, i) => (
-                          item.platform === 'facebook' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg ml-1"><i className="fab fa-facebook-f"></i></a> :
-                            item.platform === 'twitter' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg ml-1"><i className="fab fa-twitter"></i></a> :
-                              item.platform === 'instagram' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg ml-1"><i className="fab fa-instagram"></i></a> :
-                                item.platform === 'linkedin' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg ml-1"><i className="fab fa-linkedin-in"></i></a> :
-                                  item.platform === 'youtube' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg ml-1"><i className="fab fa-youtube"></i></a> :
+                          item.platform === 'facebook' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg mr-1"><i className="fab fa-facebook-f"></i></a> :
+                            item.platform === 'twitter' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg mr-1"><i className="fab fa-twitter"></i></a> :
+                              item.platform === 'instagram' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg mr-1"><i className="fab fa-instagram"></i></a> :
+                                item.platform === 'linkedin' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg mr-1"><i className="fab fa-linkedin-in"></i></a> :
+                                  item.platform === 'youtube' ? <a key={i} href={item.url} className="btn btn-floating btn-warning btn-lg mr-1"><i className="fab fa-youtube"></i></a> :
                                     null
                         ))}
                       </div>
                     </div>
                     <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-                      {/* <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
-
+                      <h5 className="text-uppercase mb-4 pb-1">ابحث عن شيء</h5>
                       <div className="form-outline form-white mb-4">
                         <input type="text" id="formControlLg" className="form-control form-control-lg" />
-                        <label className="form-label" htmlFor="formControlLg" style={{ marginLeft: 0 }}>Search</label>
-                      </div> */}
-
-                      <ul className="fa-ul" style={{ marginLeft: "1.65em" }}>
+                        <label className="form-label" htmlFor="formControlLg" style={{ marginRight: 0 }}>بحث</label>
+                      </div>
+                      <ul className="fa-ul" style={{ marginRight: "1.65em" }}>
                         {restaurantData.address && (
                           <li className="mb-3">
                             <span className="fa-li"><i className="fas fa-home"></i></span>
@@ -89,35 +60,29 @@ const Footer = () => {
                           </li>
                         )}
                       </ul>
-
                     </div>
-
                     <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
                       <h5 className="text-uppercase mb-4">مواعيد العمل</h5>
                       <table className="table text-center text-white">
                         <tbody className="font-weight-normal">
-                          {restaurantData.opening_hours ? restaurantData.opening_hours.map(item =>
-                            <tr>
+                          {restaurantData.opening_hours ? restaurantData.opening_hours.map((item, index) => (
+                            <tr key={index}>
                               <td>{item.day}:</td>
-                              {
-                                item.closed ?
-                                  <td>مغلق</td>
-                                  : <td>{item.from} - {item.to}</td>
-                              }
+                              <td>{item.closed ? 'مغلق' : `${item.from} - ${item.to}`}</td>
                             </tr>
-                          ) : ''}
+                          )) : ''}
                         </tbody>
                       </table>
                     </div>
                   </div>
                 </div>
-
                 <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-                  © 2020 Copyright:
+                  © 2020 حقوق النشر:
                   <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
                 </div>
               </footer>
             </div>
+
 
             // </footer>
           )
