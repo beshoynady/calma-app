0c425a57266d5a33264f4b7e3d73861c199f849b
bishoin2017@gmail.com <bishoin2017@gmail.com>
2024-05-26 01:16:24 +0300

    getEmployeesById

--- client/src/screens/management/manag.component/employees/Permissions.jsx
+++ client/src/screens/management/manag.component/employees/Permissions.jsx
@@ -214,6 +214,7 @@ const PermissionsComponent = () => {
           null;
 
         setpermissionEmployee(permissionEmployee);
+        console.log({ permissionEmployee });
         console.log({ selectedEmployees });
       } else {
         setselectedEmployee(null);
@@ -234,6 +235,9 @@ const PermissionsComponent = () => {
 
         const permissionEmployee = permissionsList ? permissionsList.find(permission => permission.employee === selectedEmployee._id) : null;
         setpermissionEmployee(permissionEmployee);
+        console.log({ permissionEmployee });
+        console.log({ selectedEmployee });
+
       }
     }
   };
