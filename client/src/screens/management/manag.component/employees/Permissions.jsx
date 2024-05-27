d00bbccb250fa12177061cb658ff339280aa7e82
bishoin2017@gmail.com <bishoin2017@gmail.com>
2024-05-27 03:11:16 +0300

    resourcePermissions

--- client/src/screens/management/manag.component/employees/Permissions.jsx
+++ client/src/screens/management/manag.component/employees/Permissions.jsx
@@ -318,7 +318,7 @@ const PermissionsComponent = () => {
                     </thead>
                     <tbody>
                       {permissionsListAr.map((permission, i) => {
-                        const resourcePermissions = Array.isArray(permissionEmployee) ? permissionEmployee.filter(per => per.resource === permissionsListEn[i]) : [];
+                        const resourcePermissions = Array.isArray(permissionEmployee.Permissions) ? permissionEmployee.Permissions.filter(per => per.resource === permissionsListEn[i]) : [];
                         const hasResourcePermissions = resourcePermissions.length > 0;
                         const permissionDetails = hasResourcePermissions ? resourcePermissions[0] : {};
                         console.log({resourcePermissions, permissionDetails})
