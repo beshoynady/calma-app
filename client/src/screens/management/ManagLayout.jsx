bf90ca41561d83701cbd4bb9c6c0c829e162709d
bishoin2017@gmail.com <bishoin2017@gmail.com>
2024-05-28 18:14:38 +0300

    ManagLayout

--- client/src/screens/management/ManagLayout.jsx
+++ client/src/screens/management/ManagLayout.jsx
@@ -7,6 +7,7 @@ import SideBar from './manag.component/sidebar/SideBar';
 import jwt_decode from "jwt-decode";
 import io from 'socket.io-client';
 import { ToastContainer } from 'react-toastify';
+import LoadingPage from './manag.component/LoadingPage/LoadingPage';
 
 
 const ManagLayout = () => {
@@ -34,7 +35,7 @@ const ManagLayout = () => {
                     </div>)
                 } else {
                   return (
-                    <LoadingPage />
+                    <LoadingPage/>
                   )
                 }
               }}
