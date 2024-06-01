0fbec5598bfcf1671ba0c85f7b607d04913eaabd
beshoynady <bishoin2017@gmail.com>
2024-06-01 14:38:19 +0200

    product.sizes

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -120,12 +120,10 @@ const MenuCard = () => {
                               <div className="col-md-8 btn-group btn-group-toggle" data-toggle="buttons">
                                 {product.sizes.length>0 && product.sizes.map((size, i) => {
                                   {console.log({size})}
-                                  (
-                                  <label key={i} className="d-flex justify-content-center align-items-center col-sm-4 btn btn-outline-secondary btn-sm" style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} onClick={() => handleSizeClick(size)}>
+                                  <label key={i} className={`d-flex justify-content-center align-items-center col-sm-4 btn btn-outline-secondary btn-sm ${size._id===sizeId? active : i===0 ?active:''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id===sizeId? true : i===0?true :false} onClick={() => handleSizeClick(size)}>
 
                                     <input type="radio" name="size" id={`sizeS${i}`} />{size.sizeName}
                                   </label>
-                                  )
                                 })}
                               </div>
 
