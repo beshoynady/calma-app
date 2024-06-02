06406fbe21b09006f5274fe2a565f7b90d1b14a1
beshoynady <bishoin2017@gmail.com>
2024-06-02 13:48:55 +0200

    addNoteToProduct

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -84,6 +84,11 @@ const MenuCard = () => {
                   if (product.hasSizes) {
                     return (
                       <div className="card mx-auto" key={index} style={{ maxWidth: "320px", width: "100%", height: '200px' }}>
+
+                        {product._id === productid && noteArea === true ? 
+                        <form onSubmit={(e) => { addNoteToProduct(e, product._id, sizeId); setnoteArea(!noteArea); }} className="position-absolute w-100 h-100 top-0 end-0 p-3 bg-white rounded-3 d-flex flex-column align-items-center justify-content-center"><textarea placeholder='اضف تعليماتك الخاصة بهذا الطبق' name="note" cols="100" rows="3" onChange={(e) => { setproductNote(e.target.value) }} className="w-95 h-70 my-1 z-index-1"></textarea><div className='note-btn d-flex align-items-center justify-content-center w-100 h-20 mt-2'><button className="w-45 h-100 text-light bg-dark rounded-2 me-2">تاكيد</button><button onClick={() => setnoteArea(!noteArea)} className="w-45 h-100 text-light bg-dark rounded-2">الغاء</button></div></form> 
+                        : ''}
+
                         <div className="row g-0 h-100">
                           <div className="col-5 d-flex flex-column justify-content-between">
                             <img src={defaultsImage} className="h-100 w-100" alt="Delicious soup" />
@@ -115,13 +120,13 @@ const MenuCard = () => {
                             <p className="card-text mb-2">{product.description}</p>
 
                             <div className="d-flex row justify-content-between align-items-center mb-2">
-                              <div className="col-8 btn-group btn-group-toggle" style={{direction:'ltr'}} data-toggle="buttons">
-                                {product.sizes.length>0 && product.sizes?.map((size, i) => {
-                                  return(
-                                  <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-secondary btn-sm ${size._id===sizeId? "active" : i===0 ?"active":''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id===sizeId? true : i===0?true :false} onClick={() => handleSizeClick(size)}>
-                                    <input type="radio" name="size" id={`sizeS${i}`} />
-                                    {size.sizeName}
-                                  </label>
+                              <div className="col-8 btn-group btn-group-toggle" style={{ direction: 'ltr' }} data-toggle="buttons">
+                                {product.sizes.length > 0 && product.sizes?.map((size, i) => {
+                                  return (
+                                    <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-secondary btn-sm ${size._id === sizeId ? "active" : i === 0 ? "active" : ''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id === sizeId ? true : i === 0 ? true : false} onClick={() => handleSizeClick(size)}>
+                                      <input type="radio" name="size" id={`sizeS${i}`} />
+                                      {size.sizeName}
+                                    </label>
                                   )
                                 })}
                               </div>
@@ -208,6 +213,9 @@ const MenuCard = () => {
                   } else {
                     return (
                       <div className="card mx-auto" key={index} style={{ maxWidth: "320px", width: "100%", height: '200px' }}>
+                        {product._id === productid && noteArea === true ? 
+                        <form onSubmit={(e) => { addNoteToProduct(e, product._id, ''); setnoteArea(!noteArea); }} className="position-absolute w-100 h-100 top-0 end-0 p-3 bg-white rounded-3 d-flex flex-column align-items-center justify-content-center"><textarea placeholder='اضف تعليماتك الخاصة بهذا الطبق' name="note" cols="100" rows="3" onChange={(e) => { setproductNote(e.target.value) }} className="w-95 h-70 my-1 z-index-1"></textarea><div className='note-btn d-flex align-items-center justify-content-center w-100 h-20 mt-2'><button className="w-45 h-100 text-light bg-dark rounded-2 me-2">تاكيد</button><button onClick={() => setnoteArea(!noteArea)} className="w-45 h-100 text-light bg-dark rounded-2">الغاء</button></div></form> 
+                        : ''}
                         <div className="row g-0 h-100">
                           <div className="col-5 d-flex flex-column justify-content-between">
                             <img src={defaultsImage} className="h-100 w-100" alt="Delicious soup" />
