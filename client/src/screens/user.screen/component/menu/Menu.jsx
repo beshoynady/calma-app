62eab0a58643fb607294b4ba338e8b840e38c95b
beshoynady <bishoin2017@gmail.com>
2024-06-10 01:08:20 +0200

    Menu

--- client/src/screens/user.screen/component/menu/Menu.jsx
+++ client/src/screens/user.screen/component/menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-// import './Menu.css'
+import './Menu.css'
 import MenuCard from './Menu-card/Menu-card';
 import { detacontext } from '../../../../App'
 
@@ -22,18 +22,18 @@ const Menu = () => {
           <h2>قائمة الطعام</h2>
         </div>
         <div className='section-content w-100 mt-3 d-flex flex-column flex-md-row'>
-          <nav className="menu-nav w-100 d-flex flex-column flex-lg-row align-items-center align-items-lg-center justify-content-center my-2">
-            <ul className='menu-ul list-unstyled d-flex flex-column flex-lg-row align-items-center align-items-lg-center justify-content-center p-0 m-0'>
+          <nav className="menu-nav w-100 d-flex flex-column align-items-center align-items-md-start justify-content-center my-2">
+            <ul className='menu-ul list-unstyled d-flex flex-column align-items-center align-items-md-start justify-content-center p-0 m-0'>
               {allcategories.length > 0 ? (
                 allcategories.map((category, index) => (
-                  <li key={category._id} className='menu-nav-li d-flex align-items-center justify-content-center'>
+                  <li key={category._id} className='menu-nav-li d-flex align-items-center justify-content-center my-1'>
                     <a
                       href='#menu'
-                      className={`category-btn btn ${activeItem === index ? 'btn-light text-dark' : 'btn-primary'} mx-2 my-1`}
+                      className={`category-btn btn ${activeItem === index ? 'btn-light text-dark' : 'btn-primary'} mx-2`}
                       style={{ fontSize: "22px", fontWeight: "600" }}
                       onClick={() => handleCategoryClick(category._id, index)}
                     >
-                      {category.name}
+                      <span className="rotated-text">{category.name}</span>
                     </a>
                   </li>
                 ))
