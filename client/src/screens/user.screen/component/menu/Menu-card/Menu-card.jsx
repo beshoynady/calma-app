a20dddf63e01b43352698ca54accc3eee7f431cd
beshoynady <bishoin2017@gmail.com>
2024-06-09 03:48:01 +0200

    extraArea

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -228,7 +228,6 @@ const MenuCard = () => {
                               ))}
                             </div>
 
-                            {/* خيارات الأصناف */}
                             <div className="form-group d-flex flex-wrap mt-2">
                               {Array.from({ length: product.quantity }).map((_, ind) => (
                                 selectedButtonIndex === ind + 1 && (
@@ -239,7 +238,7 @@ const MenuCard = () => {
                                           type="checkbox"
                                           className="form-check-input"
                                           value={extra._id}
-                                          checked={product.extrasSelected ? product.extrasSelected[ind].extraId.includes(extra._id) : f}
+                                          // checked={product.extrasSelected ? product.extrasSelected[ind].extraId.includes(extra._id) : false}
                                           onChange={(e) => handleAddProductExtras(extra, ind)}
                                         />
                                         <label className="form-check-label mr-4" style={{ fontSize: '18px', fontWeight: '900' }}>{extra.name}</label>
