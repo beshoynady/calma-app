54c9a1a3a83ba4acf9637487f6d5c1074da3f44b
beshoynady <bishoin2017@gmail.com>
2024-06-10 03:27:48 +0200

    MenuCard

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -18,7 +18,7 @@ const MenuCard = () => {
   const [sizePriceAfterDescount, setsizePriceAfterDescount] = useState()
 
 
-  const xax = (size) => {
+  const handleSelectSize = (size) => {
     setsize(size)
     setsizeId(size._id)
     setsizeQuantity(size.sizeQuantity)
@@ -165,7 +165,7 @@ const MenuCard = () => {
                               <div className="col-8 btn-group btn-group-toggle" style={{ direction: 'ltr' }} data-toggle="buttons">
                                 {product.sizes.length > 0 && product.sizes?.map((size, i) => {
                                   return (
-                                    <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-secondary btn-sm ${size._id === sizeId ? "active" : i === 0 ? "active" : ''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id === sizeId ? true : i === 0 ? true : false} onClick={() => xax(size)}>
+                                    <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-secondary btn-sm ${size._id === sizeId ? "active" : i === 0 ? "active" : ''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id === sizeId ? true : i === 0 ? true : false} onClick={() => handleSelectSize(size)}>
                                       <input type="radio" name="size" id={`sizeS${i}`} />
                                       {size.sizeName}
                                     </label>
