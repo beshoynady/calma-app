57efe3215e0d259169cb223f3f570b895b35813d
beshoynady <bishoin2017@gmail.com>
2024-06-12 15:32:59 +0300

    extraDetails

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -254,30 +254,47 @@ const MenuCard = () => {
 
 
                                 <div className="form-group d-flex flex-wrap mt-1" style={{ width: '100%', height: '50%', padding: '0', margin: '0' }}>
-                                {Array.from({ length: product.quantity }).map((_, ind) => (
-                                  selectedButtonIndex === ind + 1 && (
-                                    <div key={ind} className="form-group w-100 h-100 d-flex flex-column align-items-start justify-content-start flex-wrap" style={{ padding: '5px', overflowY: "scroll" }}>
-                                      {product.extras.map((extra, i) => (
+                                  {Array.from({ length: product.quantity }).map((_, ind) => (
+                                    selectedButtonIndex === ind + 1 && (
+                                      <div key={ind} className="form-group w-100 h-100 d-flex flex-column align-items-start justify-content-start flex-wrap" style={{ padding: '5px', overflowY: "scroll" }}>
+                                        {product.extras && product.extras.map((extra, i) => (
+                                          <div className="form-check form-check-flat mb-1 d-flex align-items-center" key={i} style={{ width: '47%', height: '20px', paddingLeft: '5px' }}>
+                                            <input
+                                              type="checkbox"
+                                              className="form-check-input "
+                                              value={extra._id}
+                                              checked={
+                                                (productExtras && productExtras[ind] && productExtras[ind].extraDetails.some(detail => detail.extraId === extra._id)) ||
+                                                (product.extrasSelected &&
+                                                  product.extrasSelected[ind] &&
+                                                  product.extrasSelected[ind].extraDetails.some(detail => detail.extraId === extra._id))
+                                              }
+                                              onChange={(e) => handleAddProductExtras(extra, ind)}
+                                            />
+                                            <label className="form-check-label mr-4" style={{ fontSize: '14px', fontWeight: '900' }} onClick={(e) => handleAddProductExtras(extra, ind)}>{`${extra.name} - ${extra.price} ج`} </label>
+                                          </div>
+                                        ))}
+
+                                        {/* {product.extras.map((extra, i) => (
                                         <div className="form-check form-check-flat mb-1 d-flex align-items-center" key={i} style={{ width: '47%', height: '20px', paddingLeft: '5px' }}>
-                                          {console.log({extra})}
                                           <input
                                             type="checkbox"
                                             className="form-check-input "
                                             value={extra._id}
                                             checked={
-                                              (productExtras && productExtras[ind] && productExtras[ind].extraDetails.extraId.includes(extra._id)) ||
+                                              (productExtras && productExtras[ind] && productExtras[ind].extraId.includes(extra._id)) ||
                                               (product.extrasSelected &&
                                                 product.extrasSelected[ind] &&
-                                                product.extrasSelected[ind].extraDetails.extraId.includes(extra._id))
+                                                product.extrasSelected[ind].extraId.includes(extra._id))
                                             }
                                             onChange={(e) => handleAddProductExtras(extra, ind)}
                                           />
-                                          <label className="form-check-label mr-4" style={{ fontSize: '14px', fontWeight: '900' }} onClick={(e) => handleAddProductExtras(extra, ind)}>{`${extra.extraDetails.name} - ${extra.extraDetails.price} ج`} </label>
+                                          <label className="form-check-label mr-4" style={{ fontSize: '14px', fontWeight: '900' }} onClick={(e) => handleAddProductExtras(extra, ind)}>{`${extra.name} - ${extra.price} ج`} </label>
                                         </div>
-                                      ))}
-                                    </div>
-                                  )
-                                ))}
+                                      ))} */}
+                                      </div>
+                                    )
+                                  ))}
                                 </div>
                                 <div className="note-btn d-flex align-items-center justify-content-center w-100 mt-2" style={{ height: '40px' }}>
                                   <button className="btn btn-success rounded-2 me-2" style={{ width: '50%' }}>تأكيد</button>
