6f464f1e1e143c84f32cfcc4ae6fc8ca2328af17
beshoynady <bishoin2017@gmail.com>
2024-06-14 02:41:49 +0300

    extraArea

--- client/src/screens/management/manag.component/pos/POS.jsx
+++ client/src/screens/management/manag.component/pos/POS.jsx
@@ -461,7 +461,7 @@ const POS = () => {
                           <div className="card mb-3" key={index}>
 
 
-                            {product._id === productid && extraArea === true ?
+                            {product&&product._id&&product._id === productid && extraArea === true ?
                               sizeId && product.sizes.filter(size => size._id === sizeId)[0].sizeQuantity > 0 ?
                                 (<div className="position-absolute w-100 h-100 top-0 start-0 bg-white rounded-3 d-flex flex-column align-items-center justify-content-center overflow-hidden"
                                   style={{ zIndex: 10 }}>
