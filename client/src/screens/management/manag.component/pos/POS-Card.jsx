a3afe331605318bc83489fc12dcfea5bde50efc6
beshoynady <bishoin2017@gmail.com>
2024-06-13 21:17:03 +0300

    handleSelectSize

--- client/src/screens/management/manag.component/pos/POS-Card.jsx
+++ client/src/screens/management/manag.component/pos/POS-Card.jsx
@@ -25,7 +25,7 @@ const POSCard = () => {
     setsizeQuantity(size.sizeQuantity)
     setsizePrice(size.sizePrice);
     if (size.sizeDiscount > 0) {
-      setsizePriceAfterDescount(size.sizePriceAfterDiscount);
+      setsizePriceAfterDescount(size.sizePrice-size.sizeDiscount);
     }
   };
 
@@ -43,15 +43,15 @@ const POSCard = () => {
                 allProducts.filter(pro => pro.category._id === categoryid).map((product, index) => {
                   if (product.hasSizes) {
                     return (
-                      <div className="card mx-auto m-1 bg-secondary" key={index} style={{ width: '180px', height: '170px', border: '2px solid black', transition: 'transform 0.3s, border-color 0.3s' }} onClick={() => { addItemToCart(product._id, sizeId) }}>
-                        <div className="d-flex flex-column justify-content-between" style={{ width: '100%', height: '60%' }}>
+                      <div className="card mx-auto m-1 bg-secondary" key={index} style={{ width: '180px', height: '200px', border: '2px solid black', transition: 'transform 0.3s, border-color 0.3s' }} onClick={() => { addItemToCart(product._id, sizeId) }}>
+                        <div className="d-flex flex-column justify-content-between" style={{ width: '100%', height: '50%' }}>
                           <img src={defaultsImage} className="img-fluid h-100 w-100" alt="Delicious soup" style={{ objectFit: 'cover' }} />
                         </div>
                         <div className="d-flex row justify-content-between align-items-center mb-2">
-                          <div className=" btn-group btn-group-toggle" style={{ direction: 'ltr' }} data-toggle="buttons">
+                          <div className=" btn-group btn-group-toggle " style={{ direction: 'ltr' }} data-toggle="buttons">
                             {product.sizes.length > 0 && product.sizes?.map((size, i) => {
                               return (
-                                <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-secondary btn-sm ${size._id === sizeId ? "active" : i === 0 ? "active" : ''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id === sizeId ? true : i === 0 ? true : false} onClick={() => handleSelectSize(size)}>
+                                <label key={i} className={`d-flex justify-content-center align-items-center col-4 btn btn-outline-success btn-primary ${size._id === sizeId ? "active" : i === 0 ? "active" : ''}`} style={{ height: "40px", fontSize: "24px", fontWeight: "600" }} defaultChecked={size._id === sizeId ? true : i === 0 ? true : false} onClick={() => handleSelectSize(size)}>
                                   <input type="radio" name="size" id={`sizeS${i}`} />
                                   {size.sizeName}
                                 </label>
@@ -63,14 +63,14 @@ const POSCard = () => {
                         <div className="d-flex bg-secondary justify-content-between align-items-center p-0 mx-1">
                           <h5 className="card-title text-light mb-0 text-truncate" style={{ width: '60%', fontSize: '18px', fontWeight: '900' }}>{product.name}</h5>
                           <div className="text-end ">
-                            {product.discount > 0 ? (
+                          {sizePriceAfterDescount > 0  ? (
                               <>
-                                <span className="text-light fw-bold">{product.priceAfterDiscount}ج</span>
-                                <sup><del className="text-muted text-light" style={{ fontSize: '14px', fontWeight: '900' }}>{product.price}ج</del></sup>
+                                <span className="text-light fw-bold">{sizePriceAfterDescount}ج</span>
+                                <sup><del className="text-muted text-light" style={{ fontSize: '14px', fontWeight: '900' }}>{sizePrice}ج</del></sup>
 
                               </>
                             ) : (
-                              <span className="text-light fw-bold">{product.price}ج</span>
+                              <span className="text-light fw-bold">{sizePrice}ج</span>
                             )}
                           </div>
                         </div>
