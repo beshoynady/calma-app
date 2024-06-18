910fef0df68112b27f641699305fdc96187b26ea
beshoynady <bishoin2017@gmail.com>
2024-06-18 20:40:55 +0300

    kitchenorderModal

--- client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
+++ client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
@@ -1080,7 +1080,7 @@ const ManagerDash = () => {
                           <h4 className="modal-title"></h4>
                           <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                         </div>
-                        <div ref={printContainerInvoiceSplit} className="p-1 mb-7 overflow-auto printpage" style={{ width: '100%', textAlign: 'center' }}>
+                        <div ref={printContainerInvoiceSplit} className="w-100 p-1 mb-7 overflow-auto printpage" style={{ textAlign: 'center' }}>
                           {/* Invoice Header */}
                           <div className="invoice-header" style={{ backgroundColor: '#343a40', color: '#ffffff', padding: '20px', textAlign: 'center' }}>
                             <h2>{restaurantData.name}</h2>
@@ -1162,7 +1162,7 @@ const ManagerDash = () => {
                               ))}
                             </tbody>
                             <tfoot>
-                              {console.log({orderSubtotal, subtotalSplitOrder, orderTotal})}
+                              {console.log({ orderSubtotal, subtotalSplitOrder, orderTotal })}
                               <tr>
                                 <td colSpan="3">المجموع</td>
                                 <td>{orderSubtotal - subtotalSplitOrder}</td>
@@ -1221,16 +1221,16 @@ const ManagerDash = () => {
                     <div className="modal-content">
                       <form>
                         <div className="modal-header">
-                          <h4 className="modal-title"></h4>
+                          {/* <h4 className="modal-title"></h4> */}
                           <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                          <button type="button" className="btn btn-47 btn-primary" value="طباعه للشيف" onClick={(e) => handlePrintKitchen(e)}>طباعه للشيف</button>
+                          <button type="button" className="btn btn-primary" value="طباعه للشيف" onClick={(e) => handlePrintKitchen(e)}>طباعه للشيف</button>
                         </div>
-                        <div ref={printContainerKitchen} className="max-w-400px w-100 p-1 mb-7 overflow-auto printpage" style={{ maxWidth: '400px', textAlign: 'center' }}>
+                        <div ref={printContainerKitchen} className="w-100 p-1 mb-7 overflow-auto printpage" style={{ textAlign: 'center' }}>
                           <div className="col-md-4 mb-4" style={{ direction: 'rtl' }}>
                             <div className="card text-white bg-success" style={{ width: "265px" }}>
                               <div className="card-body text-right d-flex justify-content-between p-0 m-1">
                                 <div style={{ maxWidth: "50%" }}>
-                                  <p className="card-text"> {kitchenOrder.table != null ? `طاولة: ${usertitle(kitchenOrder.table)}` : (kitchenOrder.user ? `العميل: ${usertitle(kitchenOrder.user)}` : '')}</p>
+                                  <p className="card-text"> {kitchenOrder.table ? (`طاولة: ${kitchenOrder.table.tableNumber}` ): (kitchenOrder.user ? `العميل: ${usertitle(kitchenOrder.user)}` : '')}</p>
                                   <p className="card-text">نوع الطلب: {kitchenOrder.orderType}</p>
                                   {kitchenOrder.ordernum ? `<p className="card-text"> رقم الطلب:  ${kitchenOrder.ordernum} </p>` : ''}
                                 </div>
