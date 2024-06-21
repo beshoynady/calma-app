715dfa3842c6c5919548b642e52092b3d9350ac0
beshoynady <bishoin2017@gmail.com>
2024-06-21 23:03:50 +0300

    askingForHelp

--- client/src/App.js
+++ client/src/App.js
@@ -1521,47 +1521,7 @@ function App() {
 
 
 
-  const askingForHelp = async (tableId) => {
-    try {
-      // Filter orders for the specified table
-      const tableOrders = allOrders.filter(order => order.table && order.table._id == tableId);
-  
-      // Get the last order for the table
-      const lastTableOrder = tableOrders.length > 0 ? tableOrders[tableOrders.length - 1] : null;
-  
-      // Check if the last table order is active
-      const lastTableOrderActive = lastTableOrder ? await lastTableOrder.isActive : false;
-  
-      // If the last table order is not active, create a new order
-      if (!lastTableOrderActive) {
-        // Generate a new serial number for the order
-        const serial = allOrders.length > 0 ? 
-          String(Number(allOrders[allOrders.length - 1].serial) + 1).padStart(6, '0') : 
-          '000001';
-  
-        // Define the help request message
-        const help = 'Requests assistance';
-        const table = tableId;
-  
-        // Create a new order with the help request
-        const newOrder = await axios.post(`${apiUrl}/api/order/`, { serial, table, help });
-        console.log(newOrder);
-      } else {
-        // If the last table order is active, update the existing order with the help request
-        const id = lastTableOrder._id;
-        const help = 'Requests assistance';
-        const helpStatus = 'Ns'
-        const updatedOrder = await axios.put(`${apiUrl}/api/order/${id}`, { help, helpStatus});
-        console.log(updatedOrder);
-      }
-    } catch (error) {
-      // Log any errors that occur during the process
-      console.error(error);
-  
-      // Display an error toast message
-      toast.error('An error occurred while requesting assistance.');
-    }
-  };
+
   
 
 
--- client/src/screens/user.screen/component/home/Home.jsx
+++ client/src/screens/user.screen/component/home/Home.jsx
@@ -2,13 +2,66 @@ import React, { useRef, useState, useEffect } from 'react';
 import './Home.css'
 import { detacontext } from '../../../../App'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
   const {id} = useParams()
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const token = localStorage.getItem('token_e');
+  const config = {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  };
+
+
+  const askingForHelp = async (tableId) => {
+    try {
+      const allOrders = await axios.get(apiUrl+'/api/order/limit/30' ,config)
+      // Filter orders for the specified table
+      const tableOrders = allOrders.filter(order => order.table && order.table._id == tableId);
+  
+      // Get the last order for the table
+      const lastTableOrder = tableOrders.length > 0 ? tableOrders[tableOrders.length - 1] : null;
+  
+      // Check if the last table order is active
+      const lastTableOrderActive = lastTableOrder ? await lastTableOrder.isActive : false;
+  
+      // If the last table order is not active, create a new order
+      if (!lastTableOrderActive) {
+        // Generate a new serial number for the order
+        const serial = allOrders.length > 0 ? 
+          String(Number(allOrders[allOrders.length - 1].serial) + 1).padStart(6, '0') : 
+          '000001';
+  
+        // Define the help request message
+        const help = 'Requests assistance';
+        const table = tableId;
+  
+        // Create a new order with the help request
+        const newOrder = await axios.post(`${apiUrl}/api/order/`, { serial, table, help });
+        console.log(newOrder);
+      } else {
+        // If the last table order is active, update the existing order with the help request
+        const id = lastTableOrder._id;
+        const help = 'Requests assistance';
+        const helpStatus = 'Ns'
+        const updatedOrder = await axios.put(`${apiUrl}/api/order/${id}`, { help, helpStatus});
+        console.log(updatedOrder);
+      }
+    } catch (error) {
+      // Log any errors that occur during the process
+      console.error(error);
+  
+      // Display an error toast message
+      toast.error('An error occurred while requesting assistance.');
+    }
+  };
+
   return (
     <detacontext.Consumer>
       {
-        ({restaurantData, askingForHelp ,userLoginInfo, usertitle}) => {
+        ({restaurantData,userLoginInfo, usertitle}) => {
           return (
             <main className='main-home' id='main'> 
               <div className="container">
