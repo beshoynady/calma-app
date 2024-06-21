146147802ebd8ef5d58c6c442b1d6d0fcc1c2957
beshoynady <bishoin2017@gmail.com>
2024-06-22 02:45:08 +0300

    fetchActiveEmployees

--- client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
+++ client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
@@ -180,7 +180,7 @@ const ManagerDash = () => {
     try {
       const allEmployees = await axios.get(apiUrl + '/api/employee', config);
       const activeEmployees = allEmployees.data.filter((employee) => employee.isActive === true)
-      const allWaiters = activeEmployees.data.length > 0 ? activeEmployees.data.filter((employee) => employee.role === 'waiter') : [];
+      const allWaiters = activeEmployees.length > 0 ? activeEmployees.filter((employee) => employee.role === 'waiter') : [];
       if (allWaiters) {
         setAllWaiters(allWaiters);
       } else {
@@ -188,7 +188,7 @@ const ManagerDash = () => {
         toast.warning('لم يتم العثور على ويتر نشط الان.');
       }
 
-      const alldeliverymens = activeEmployees.data.length > 0 ? activeEmployees.data.filter((employee) => employee.role === 'deliveryman') : [];
+      const alldeliverymens = activeEmployees.length > 0 ? activeEmployees.filter((employee) => employee.role === 'deliveryman') : [];
       if (alldeliverymens) {
         setDeliverymen(alldeliverymens);
       } else {
@@ -871,8 +871,8 @@ const ManagerDash = () => {
                         >
                           <div className="task-title card-body d-flex justify-content-between align-items-center p-0 m-0">
                             <div className='d-flex w-50 justify-content-between align-items-center'>
-                              <p className='w-50 text-dark' style={{ fontSize: '16px', fontWeight: "900" }}>طاوله : {order.table && order.table.tableNumber}</p>
-                              <p className='w-50 text-dark' style={{ fontSize: '16px', fontWeight: "900" }}>
+                              <p className='w-50 text-dark' style={{ fontSize: '18px', fontWeight: "900" }}>طاوله : {order.table && order.table.tableNumber}</p>
+                              <p className='w-50 text-dark' style={{ fontSize: '18px', fontWeight: "900" }}>
                                 {order.help === 'Requests assistance'
                                   ? 'يحتاج المساعدة'
                                   : order.help === 'Requests bill'
@@ -891,8 +891,8 @@ const ManagerDash = () => {
                               </button>
                             ) : (
                               <div className='d-flex flex-nowrap w-50 text-dark justify-content-between align-items-center'>
-                                <p className='w-50 text-dark text-center' style={{ fontSize: '16px', fontWeight: "900" }}> {order.waiter?.fullname}</p>
-                                <p className='w-50 text-dark text-center' style={{ fontSize: '16px', fontWeight: "900" }}>{order.helpStatus === 'Send waiter' ? 'تم ارسال'
+                                <p className='w-50 text-dark text-center' style={{ fontSize: '18px', fontWeight: "900" }}> {order.waiter?.fullname}</p>
+                                <p className='w-50 text-dark text-center' style={{ fontSize: '18px', fontWeight: "900" }}>{order.helpStatus === 'Send waiter' ? 'تم ارسال'
                                   : order.helpStatus === 'On the way' ? 'في الطريق'
                                     : order.helpStatus === 'Assistance done' ? 'تمت'
                                       : ''
