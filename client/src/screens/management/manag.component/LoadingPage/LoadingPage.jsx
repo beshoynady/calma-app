69e8c1bde7304b862154952a26780a96b958226d
beshoynady <bishoin2017@gmail.com>
2024-06-26 20:54:31 +0300

    LoadingPage

--- client/src/screens/management/manag.component/LoadingPage/LoadingPage.jsx
+++ client/src/screens/management/manag.component/LoadingPage/LoadingPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './LoadingPage.css';
+import { useContext } from 'react';
+import { detacontext } from '../../../../App';
+
 
 const LoadingPage = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  const {resturantData} = useContext(detacontext)
   return (
     <div className="d-flex justify-content-center align-items-center min-vh-100">
       <div className="text-center">
-        <img src="https://via.placeholder.com/100" alt="Logo" className="logo mb-4" />
+        {resturantData.logo?
+        <img src={`${apiUrl}/images/${resturantData.logo}`} alt="Logo" className="logo mb-4" />
+        :''
+      }
         <div className="spinner"></div>
-        <div className="loading-text mt-3">الرجاء الانتظار...</div>
+        <div className="loading-text mt-3" >الرجاء الانتظار...</div>
       </div>
     </div>
   );
