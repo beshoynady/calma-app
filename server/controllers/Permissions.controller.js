858e3171088db60ba1fffe9ba004adf427beca9f
beshoynady <bishoin2017@gmail.com>
2024-06-29 14:57:49 +0300

    createPermission

--- server/controllers/Permissions.controller.js
+++ server/controllers/Permissions.controller.js
@@ -29,7 +29,7 @@ const createPermission = async (req, res) => {
       res.status(201).json(newPermission);
     } catch (error) {
       console.error('Error creating permission:', error);
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message, error });
     }
   };
   
